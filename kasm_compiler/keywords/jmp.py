38a369d9d779e5713abc6501e9da88eb9863d239
klinefelters <klinefelters@etown.edu>
2025-03-27 00:16:31 -0400

    feat: add WIO and RIO instructions, enhance JMP functionality with new jump conditions

--- kasm_compiler/keywords/jmp.py
+++ kasm_compiler/keywords/jmp.py
@@ -10,11 +10,66 @@ class JMP(BaseKeyword):
     jump the program counter to the value in register Rb
     """
     keyword: str = 'JMP'
+    sel: str = '111'
     
     def parse(self, args):
         if type(args[0]) is Register:
             rb= args[0]
         else:
-            raise Exception("JMP requires a register as arguments")
+            raise Exception("JMP requires a register as an argument")
 
-        return '101' + '1110' + '000' + '000' + rb.parse()
+        return '101' + self.sel + '0' + '000' + '000' + rb.parse()
+    
+@define
+class JMPGRT(BaseKeyword):
+    """
+    JMPGRT [Rb]
+    jump the program counter to the value in register Rb if a was greater than b
+    """
+    keyword: str = 'JMPGRT'
+    sel: str = '000'
+
+@define
+class JMPEQL(BaseKeyword):
+    """
+    JMPEQL [Rb]
+    jump the program counter to the value in register Rb if a was equal to b
+    """
+    keyword: str = 'JMPEQL'
+    sel: str = '001'
+
+@define
+class JMPLST(BaseKeyword):
+    """
+    JMPLST [Rb]
+    jump the program counter to the value in register Rb if a was less than b
+    """
+    keyword: str = 'JMPLST'
+    sel: str = '010'
+
+@define
+class JMPUGT(BaseKeyword):
+    """
+    JMPUGT [Rb]
+    jump the program counter to the value in register Rb if unsigned a was greater than unsigned b
+    """
+    keyword: str = 'JMPUGT'
+    sel: str = '011'
+
+@define
+class JMPUEQ(BaseKeyword):
+    """
+    JMPUEQ [Rb]
+    jump the program counter to the value in register Rb if unsigned a was equal to unsigned b
+    """
+    keyword: str = 'JMPUEQ'
+    sel: str = '100'
+
+@define
+class JMPULT(BaseKeyword):
+    """
+    JMPULT [Rb]
+    jump the program counter to the value in register Rb if unsigned a was less than unsigned b
+    """
+    keyword: str = 'JMPULT'
+    sel: str = '101'
